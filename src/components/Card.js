4d6737dc26ac06aad1158d863be59c5d9041726a
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-03 03:10:52 +0530

    this is my two hundred fifteen commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,12 @@
-function Card(){
-    return();
+function Card({id,image,info,price,name}){
+    return(<div className="card">
+        <img src={image} className="image"></img>
+        <div className="tourinfo">
+            <h4 className="tour-price">{price}</h4>
+            <h4 className="tour-name">{name}</h4>
+        </div>
+        <div className="description"></div>
+    </div>);
 
 }
 export default Card;
