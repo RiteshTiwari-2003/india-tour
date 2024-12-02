f6d410cb55d9d59229f328255d892b3163a5c97a
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-02 22:33:58 +0530

    this is my two hundred ten commit

--- src/App.js
+++ src/App.js
@@ -5,3 +5,5 @@ const App = () => {
 };
 
 export default App;
+// for running this project first go on terminal and run mpm i or npm install for installing all nam packages and node module and package.json whihc is basically meta data of your project in which all basic information present about project
+// after this for running the program run the npm start
