f1fceb48d8575363902687de787e607bfb821b7f
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-04 01:40:22 +0530

    this is my two hundred thirty two commit

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,10 +1,17 @@
 import Card from "./Card"
 function Tours({tours,removeTour}){
-    return(<div><div><h2>Plan With Friend</h2></div>
-    <div>{
-        tours.map((tour)=>{
-            return <Card key={tour.id}{...tour} removeTours={removeTour}></Card>
-        })}</div></div>);
+    return(
+        <div className="container">
+            <div>
+                <h2 className="title">Plan With Friend</h2>
+            </div>
+            <div className="cards">
+                {tours.map((tour)=>{
+                    return <Card key={tour.id}{...tour} removeTour={removeTour}></Card>
+                })}
+            </div>
+        </div>
+    );
 
 }
 export default Tours;
